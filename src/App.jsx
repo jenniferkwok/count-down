5fb22a11c84d902326a9cd9f7b104816bbad4a46
jenniferkwok <jenwkwok@gmail.com>
2018-11-24 01:35:31 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+	import React, { Component } from 'react';
+	import './App.css';
+
+	class App extends Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				deadline: 'January 1, 2019'
+			}
+		}
+		render() {
+			return(
+			<div className="App">
+				<div className="App-title">
+				CoundDown to {this.state.deadline}!</div>
+				<div>
+					<div className="Clock-days">14 days</div>
+					<div className="Clock-hours">20 hours</div>
+					<div className="Clock-minutes">10 minutes</div>
+					<div className="Clock-seconds">50 seconds</div>
+				</div>
+				<div>
+				<input placeholder='new date'/>
+				<button>Submit</button>
+				</div>
+			</div>
+				)
+			}
+		}
+
+		export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+	// <div>countdown</div>, 
+	<App />,
+	document.getElementById('root')
+	);
